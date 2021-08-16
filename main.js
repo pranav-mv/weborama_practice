e3ac5b7a9b737684c73b8bfc5d5ca19616c94356
Pranav MV <84380561+pranav-mv@users.noreply.github.com>
2021-08-16 20:45:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+a=0
+function add(){
+    a++
+
+document.getElementById('screen').innerText=a
+}
+function sub(){
+    a--
+
+document.getElementById('screen').innerText=a
+}
+function reset(){
+    a=0
+
+document.getElementById('screen').innerText=a
+}
